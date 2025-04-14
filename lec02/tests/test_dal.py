129a4e9f1b58ce7177f3fdbe886f9bffa1771c15
Avdiienko Illia <avil48job@gmail.com>
2025-04-14 14:14:43 +0300

    test(DAL): use fixtures for clean-up since it's way more convenient and helps to avoid repeating ourselves

--- lec02/tests/test_dal.py
+++ lec02/tests/test_dal.py
@@ -4,41 +4,49 @@ from pathlib import Path
 from shutil import rmtree as remove_content_recursively_from
 
 from assertpy import assert_that
+from pytest import fixture
 
 from dal.storager import dump_to_folder, resolve_path_to, refresh_storage
 
 
+@fixture
+def temporary_storage():
+    test_storage: Path = resolve_path_to("../test_storage")
+    test_storage.mkdir(parents=True, exist_ok=True)
+    yield test_storage
+
+    ...
+
+    remove_content_recursively_from(test_storage)
+
+
 def create_dummy_json_file_inside(target_dir: Path, identifier: int) -> None:
     fake_data = {
-            "client": "We do not care",
-            "purchase_date": "about whatever",
-            "product": "are contents here",
-            "price": identifier * 200,
-        },
+        "client": "We do not care",
+        "purchase_date": "about whatever",
+        "product": "are contents here",
+        "price": identifier * 200,
+    }
     file_path = target_dir / f"test_file_{identifier}.json"
     with open(file_path, "w", encoding="utf-8") as file:
         dump_json(fake_data, file)
 
 
-def test_storage_directory_is_cleaned_up_on_refresh():
-    test_storage: Path = resolve_path_to("../test_storage")
-    test_storage.mkdir(parents=True, exist_ok=True)
+def test_storage_directory_is_cleaned_up_on_refresh(temporary_storage: Path):
+    create_dummy_json_file_inside(temporary_storage, 1)
+    create_dummy_json_file_inside(temporary_storage, 2)
+    create_dummy_json_file_inside(temporary_storage, 3)
+    create_dummy_json_file_inside(temporary_storage, 4)
 
-    create_dummy_json_file_inside(test_storage, 1)
-    create_dummy_json_file_inside(test_storage, 2)
-    create_dummy_json_file_inside(test_storage, 3)
-    create_dummy_json_file_inside(test_storage, 4)
-    directory_contents_before_cleanup = list(test_storage.iterdir())
+    directory_contents_before_cleanup = list(temporary_storage.iterdir())
     assert_that(directory_contents_before_cleanup).is_not_empty()
 
-    refresh_storage(test_storage)
-    directory_contents_after_cleanup = list(test_storage.iterdir())
+    refresh_storage(temporary_storage)
+    directory_contents_after_cleanup = list(temporary_storage.iterdir())
     assert_that(directory_contents_after_cleanup).is_empty()
 
-    test_storage.rmdir()
 
-
-def test_file_is_created_correctly():
+def test_file_is_created_correctly(temporary_storage: Path):
     fake_data = [
         {
             "client": "Michael Wilkerson",
@@ -59,12 +67,13 @@ def test_file_is_created_correctly():
             "price": 1042,
         },
     ]
-    storage = dump_to_folder(fake_data, "../test_storage/raw")
-    assert_that(storage).is_file()
+    raw_dir = temporary_storage / "raw"
+    raw_dir.mkdir(parents=True, exist_ok=True)
+    storage = dump_to_folder(fake_data, str(raw_dir))
+
     assert_that(storage).is_not_empty()
+    assert_that(storage).is_file()
 
     with open(storage, "r", encoding="utf-8") as file:
         written_data = read_json_from(file)
         assert_that(written_data).is_equal_to(fake_data)
-
-    remove_content_recursively_from(Path(storage).resolve().parent.parent)
