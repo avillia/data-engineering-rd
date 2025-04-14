09cffb43df99fb3a3918d54570b9e9a5ad945083
Avdiienko Illia <avil48job@gmail.com>
2025-04-14 11:32:15 +0300

    test(DAL): enhance tests so they ensure file clean-up and data consistency

--- lec02/tests/test_dal.py
+++ lec02/tests/test_dal.py
@@ -1,6 +1,41 @@
+from json import dump as dump_json
+from json import load as read_json_from
+from pathlib import Path
+from shutil import rmtree as remove_content_recursively_from
+
 from assertpy import assert_that
 
-from dal.extractor import dump_to_folder
+from dal.extractor import dump_to_folder, resolve_path_to, refresh_storage
+
+
+def create_dummy_json_file_inside(target_dir: Path, identifier: int) -> None:
+    fake_data = {
+            "client": "We do not care",
+            "purchase_date": "about whatever",
+            "product": "are contents here",
+            "price": identifier * 200,
+        },
+    file_path = target_dir / f"test_file_{identifier}.json"
+    with open(file_path, "w", encoding="utf-8") as file:
+        dump_json(fake_data, file)
+
+
+def test_storage_directory_is_cleaned_up_on_refresh():
+    test_storage: Path = resolve_path_to("../test_storage")
+    test_storage.mkdir(parents=True, exist_ok=True)
+
+    create_dummy_json_file_inside(test_storage, 1)
+    create_dummy_json_file_inside(test_storage, 2)
+    create_dummy_json_file_inside(test_storage, 3)
+    create_dummy_json_file_inside(test_storage, 4)
+    directory_contents_before_cleanup = list(test_storage.iterdir())
+    assert_that(directory_contents_before_cleanup).is_not_empty()
+
+    refresh_storage(test_storage)
+    directory_contents_after_cleanup = list(test_storage.iterdir())
+    assert_that(directory_contents_after_cleanup).is_empty()
+
+    test_storage.rmdir()
 
 
 def test_file_is_created_correctly():
@@ -24,6 +59,12 @@ def test_file_is_created_correctly():
             "price": 1042,
         },
     ]
-    storage = dump_to_folder(fake_data)
+    storage = dump_to_folder(fake_data, "../test_storage/raw")
     assert_that(storage).is_file()
     assert_that(storage).is_not_empty()
+
+    with open(storage, "r", encoding="utf-8") as file:
+        written_data = read_json_from(file)
+        assert_that(written_data).is_equal_to(fake_data)
+
+    remove_content_recursively_from(Path(storage).resolve().parent.parent)
