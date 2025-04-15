a9dad6348834bb1b0611661ec10c52fc748bef05
Avdiienko Illia <avil48job@gmail.com>
2025-04-15 18:15:46 +0300

    fix(bll): readjust business logic to match source API specification

--- lec02/bll/extractor.py
+++ lec02/bll/extractor.py
@@ -1,9 +1,21 @@
 from datetime import date as Date
 
-from dal.fetcher import get_sales
-from dal.storager import dump_to_folder
+from lec02.dal.fetcher import get_sales
+from lec02.dal.storager import dump_to_raw_folder
 
 
-def fetch_sales_api(date: Date, api_url: str, api_token: str, raw_dir: str) -> str:
-    data = get_sales(date, api_url, api_token)
-    return dump_to_folder(data, raw_dir)
+def fetch_sales_api(date: Date, api_url: str, api_token: str, file_storage: str) -> str:
+    endpoint = "sales"
+    date_as_str = date.strftime("%Y-%m-%d")
+    data = get_sales(
+        date_as_str,
+        api_url + endpoint,
+        api_token,
+    )
+    file_name = dump_to_raw_folder(
+        data,
+        file_storage,
+        date_as_str,
+        endpoint,
+    )
+    return file_name
--- lec02/dal/fetcher.py
+++ lec02/dal/fetcher.py
@@ -1,18 +1,29 @@
-from datetime import date as Date
-
 from requests import get as run_get_request
 
 
-def get_sales(date: Date, api_url: str, api_token: str) -> list[dict]:
+def get_sales(date: str, api_url: str, api_token: str) -> list[dict]:
     auth_headers = {
         "Authorization": api_token,
     }
 
-    response = run_get_request(
-        api_url,
-        headers=auth_headers,
-        params={"date": str(date)},
-    )
-    response.raise_for_status()
+    sales_data: list[dict] = []
 
-    return response.json()
+    page = 1
+    still_something_to_fetch = True
+    while still_something_to_fetch:
+        response = run_get_request(
+            api_url,
+            headers=auth_headers,
+            params={
+                "date": date,
+                "page": page,
+            },
+        )
+        if response.status_code == 200:
+            sales_data.extend(response.json())
+            page += 1
+            still_something_to_fetch = True
+        if response.status_code == 404:
+            still_something_to_fetch = False
+
+    return sales_data
