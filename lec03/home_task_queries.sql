1643590354f899ab08470659d8419f2448d1403b
Avdiienko Illia <avil48job@gmail.com>
2025-04-18 21:40:54 +0300

    feat(first-task): sql request implemented!

--- lec03/home_task_queries.sql
+++ lec03/home_task_queries.sql
@@ -9,7 +9,13 @@
 Результат відсортувати за спаданням.
 */
 -- SQL code goes here...
-
+SELECT
+    COUNT(film_category.film_id) as amount_of_films,
+    category.name AS category_name
+FROM film_category
+JOIN public.category on film_category.category_id = category.category_id
+GROUP BY category.name
+ORDER BY amount_of_films DESC;
 
 
 /*
