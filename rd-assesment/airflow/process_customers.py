a6f32357249f260da7404681d83bc9ac1107cfb9
avillia <avil48job@gmail.com>
2025-06-27 15:09:53 +0300

    feat(silver): implement DAGs for customers data crawling

--- rd-assesment/airflow/process_customers.py
+++ rd-assesment/airflow/process_customers.py
@@ -1,9 +1,9 @@
 from datetime import datetime
 
 from airflow import DAG
+from airflow.providers.standard.operators.python import PythonOperator
 from airflow.providers.amazon.aws.hooks.glue_crawler import GlueCrawlerHook
 from airflow.providers.amazon.aws.operators.glue import GlueJobOperator
-from airflow.providers.standard.operators.python import PythonOperator
 
 
 def start_glue_crawler(crawler_name):
@@ -16,24 +16,24 @@ default_args = {
     "start_date": datetime(2025, 1, 1),
 }
 
-MIDNIGHT_DAILY = "0 0 * * *"
+AT_2_AM_UTC_MIDNIGHT = "0 2 * * *"
 
 with DAG(
-    "process_sales",
+    "process_customers",
     default_args=default_args,
-    schedule=MIDNIGHT_DAILY,
+    schedule=AT_2_AM_UTC_MIDNIGHT,
     catchup=False,
 ) as dag:
     run_crawler = PythonOperator(
-        task_id="run_sales_crawler",
+        task_id="run_customers_crawler",
         python_callable=start_glue_crawler,
-        op_kwargs={"crawler_name": "sales_bronze_crawler"},
+        op_kwargs={"crawler_name": "customers_bronze_crawler"},
     )
 
     glue_etl_job = GlueJobOperator(
-        task_id="bronze_to_silver",
-        job_name="sales_bronze_to_silver",
-        script_location="s3://<DataLakeBucket>/scripts/glue_sales_etl.py",
+        task_id="bronze_to_silver_customers",
+        job_name="customers_bronze_to_silver",
+        script_location="s3://<DataLakeBucket>/scripts/glue_customers_etl.py",
         region_name="us-east-1",
         iam_role_name="<GlueServiceRole>",
         script_args={"--BUCKET_NAME": "<DataLakeBucket>"},
