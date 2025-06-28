afcd38606a8b551ec52d0c76a5151c7fc0984631
avillia <avil48job@gmail.com>
2025-06-28 17:10:11 +0300

    fix(enrich-profiles): make enrich-user-profiles run on process-user-profiles completion

--- rd-assesment/airflow/process_user_profiles.py
+++ rd-assesment/airflow/process_user_profiles.py
@@ -37,4 +37,14 @@ with DAG(
         aws_conn_id="aws_default",
     )
 
-    run_crawler >> glue_etl_job
+    glue_enrichment_job = GlueJobOperator(
+        task_id="silver_to_gold_enrich_profiles",
+        job_name="enrich_user_profiles_job",
+        script_location="s3://<DataLakeBucket>/scripts/enrich_user_profiles.py",
+        region_name="us-east-1",
+        iam_role_name="<GlueServiceRole>",
+        script_args={"--BUCKET_NAME": "<DataLakeBucket>"},
+        aws_conn_id="aws_default",
+    )
+
+    run_crawler >> glue_etl_job >> glue_enrichment_job
